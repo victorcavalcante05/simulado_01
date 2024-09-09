920f8bc7f54ab292e06adfcfcf3b566d2e18e93f
Laerte Jun Takeuti <laertejt@gmail.com>
2024-09-09 11:40:35 -0300

    readme

--- ex_dict_list_funcao.py
+++ ex_dict_list_funcao.py
@@ -1,44 +1,18 @@
 # Exercicio para alunos de administracao e economia
 # Evento Gastronomico realizado na faculdade para arrecadar fundos para uma entidade filantrópica
 #Lista dos organizadores do evento
-lst_organizadores = ['luigi', 'ludmila', 'emanuel']
+lst_organizadores = ['bento','mariana','saulo']
 # Fornecedores do evento
 dict_fornecedor = {
     0: {'nome': 'Pastel Frito', 'vendas': 3000},
     1: {'nome': 'Massa da Mama', 'vendas': 5000},
     2: {'nome': 'Pizza do Tio', 'vendas': 10000},
     3: {'nome': 'Rodizio Mineiro', 'vendas': 3900},
-    4: {'nome': 'Restaurante para Todos', 'vendas': 6000},
-    5: {'nome': 'Bom Prato', 'vendas': 9000},
-    6: {'nome': 'Yakisoba Delicia', 'vendas': 3000},
-    7: {'nome': 'Doceria Feliz', 'vendas': 8000},
-    8: {'nome': 'Bolo Gostoso', 'vendas': 8900},
-    9: {'nome': 'Carne Assada', 'vendas': 7400},
-    10: {'nome': 'Panqueca na Hora', 'vendas': 10743},
-    11: {'nome': 'Quibe Bem Feito', 'vendas': 8733},
-    12: {'nome': 'Coxinha da Tia', 'vendas': 2999},
-    13: {'nome': 'Brigadeiro do Ceu', 'vendas': 4500},
-    14: {'nome': 'Feijoada Preta', 'vendas': 9022},
-    15: {'nome': 'Sushi Baiano', 'vendas': 8444},
-    16: {'nome': 'Peixe Grelhado', 'vendas': 19000},
-    17: {'nome': 'Porco no Rolete', 'vendas': 4000},
-    18: {'nome': 'Lanche Seja Feliz', 'vendas': 8000}
-    }
+}
 # Clientes que consumiram no evento
 list_cliente = ['rafael','douglas','amanda','jorge','jeffri','carol','cristian',
  'leonardo','maria','luana','vanessa','jose','maria','bento','mariana','saulo',
- 'glauco','ze','maria','sandra','joao','mauro','tulio','romario','bebeto',
- 'aldair','jose roberto','izabela','parreira','glauber','kim','arthur','leticia',
- 'mario','solange','marcio','ludmila','luigi','emanuel','adriana','alan','lucas',
- 'vitor','vitoria','zeca','tafarel','marcao','zuleica','abraao','davi','pedro',
- 'bola','miguel','camila','xandao','alexandre','lula','zelia','mateus','almir',
- 'daniel','daniela','nelio','collor','fabio','fabiana','ramalho','ducan',
- 'mike','lebron','senna','pablo','pamela','pipen','gonçalo','piki',
- 'nubia','eiji','otavio','patricia','elen','icaro','yumi','rafael','wilson',
- 'fabricio','wagner','heitor', 'sandro', 'glaucia', 'george', 'pierre',
- 'fabio', 'wanderley', 'wesley', 'beatriz', 'ivete', 'luciano', 'ricardo',
- 'thome', 'josue', 'mateus', 'evandro', 'hellen', 'valber', 'zuleika', 'edvaldo',
- 'galvao', 'eduardo', 'silvia','galileu','morais', 'claudia','nilvia']
+ 'glauco','ze','maria']
 # Despesas do evento
 locacao = 0.10 # 10% das vendas
 limpeza = 700
@@ -58,7 +32,7 @@ outras_despesas = 500
 # 4 - Adicionar essa lista dos professores na lista de clientes
 
 
-# 5 - Calcular quantos pessoas estiveram e a media de gasto por pessoa
+# 5 - Calcular quantas pessoas estiveram e a media de gasto por pessoa
 
 
 # 6 - Encontrar quem foi o 10 consumidor e retire da lista
