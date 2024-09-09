1da624228aec567f787bdc3f81930880105aa26e
Laerte Jun Takeuti <laertejt@gmail.com>
2024-09-09 10:32:10 -0300

    innitial commit

---
+++ ex_dict_list_funcao.py
@@ -0,0 +1,91 @@
+# Exercicio para alunos de administracao e economia
+# Evento Gastronomico realizado na faculdade para arrecadar fundos para uma entidade filantrópica
+#Lista dos organizadores do evento
+lst_organizadores = ['luigi', 'ludmila', 'emanuel']
+# Fornecedores do evento
+dict_fornecedor = {
+    0: {'nome': 'Pastel Frito', 'vendas': 3000},
+    1: {'nome': 'Massa da Mama', 'vendas': 5000},
+    2: {'nome': 'Pizza do Tio', 'vendas': 10000},
+    3: {'nome': 'Rodizio Mineiro', 'vendas': 3900},
+    4: {'nome': 'Restaurante para Todos', 'vendas': 6000},
+    5: {'nome': 'Bom Prato', 'vendas': 9000},
+    6: {'nome': 'Yakisoba Delicia', 'vendas': 3000},
+    7: {'nome': 'Doceria Feliz', 'vendas': 8000},
+    8: {'nome': 'Bolo Gostoso', 'vendas': 8900},
+    9: {'nome': 'Carne Assada', 'vendas': 7400},
+    10: {'nome': 'Panqueca na Hora', 'vendas': 10743},
+    11: {'nome': 'Quibe Bem Feito', 'vendas': 8733},
+    12: {'nome': 'Coxinha da Tia', 'vendas': 2999},
+    13: {'nome': 'Brigadeiro do Ceu', 'vendas': 4500},
+    14: {'nome': 'Feijoada Preta', 'vendas': 9022},
+    15: {'nome': 'Sushi Baiano', 'vendas': 8444},
+    16: {'nome': 'Peixe Grelhado', 'vendas': 19000},
+    17: {'nome': 'Porco no Rolete', 'vendas': 4000},
+    18: {'nome': 'Lanche Seja Feliz', 'vendas': 8000}
+    }
+# Clientes que consumiram no evento
+list_cliente = ['rafael','douglas','amanda','jorge','jeffri','carol','cristian',
+ 'leonardo','maria','luana','vanessa','jose','maria','bento','mariana','saulo',
+ 'glauco','ze','maria','sandra','joao','mauro','tulio','romario','bebeto',
+ 'aldair','jose roberto','izabela','parreira','glauber','kim','arthur','leticia',
+ 'mario','solange','marcio','ludmila','luigi','emanuel','adriana','alan','lucas',
+ 'vitor','vitoria','zeca','tafarel','marcao','zuleica','abraao','davi','pedro',
+ 'bola','miguel','camila','xandao','alexandre','lula','zelia','mateus','almir',
+ 'daniel','daniela','nelio','collor','fabio','fabiana','ramalho','ducan',
+ 'mike','lebron','senna','pablo','pamela','pipen','gonçalo','piki',
+ 'nubia','eiji','otavio','patricia','elen','icaro','yumi','rafael','wilson',
+ 'fabricio','wagner','heitor', 'sandro', 'glaucia', 'george', 'pierre',
+ 'fabio', 'wanderley', 'wesley', 'beatriz', 'ivete', 'luciano', 'ricardo',
+ 'thome', 'josue', 'mateus', 'evandro', 'hellen', 'valber', 'zuleika', 'edvaldo',
+ 'galvao', 'eduardo', 'silvia','galileu','morais', 'claudia','nilvia']
+# Despesas do evento
+locacao = 0.10 # 10% das vendas
+limpeza = 700
+seguranca = 800
+outras_despesas = 500
+
+# 1 - Calcular o total de vendas usando o for loop no dict
+
+       
+# Os professores doaram 5000,00, cada professor doou 1000,00
+# 2 - Criar um dicionario com o nome e valor
+
+
+# 3 - Criar uma lista com os nomes dos professores
+
+
+# 4 - Adicionar essa lista dos professores na lista de clientes
+
+
+# 5 - Calcular quantos pessoas estiveram e a media de gasto por pessoa
+
+
+# 6 - Encontrar quem foi o 10 consumidor e retire da lista
+
+
+# 7 - Encontre o nome "bola" inserido incorretamente na lista e retire da lista
+
+
+# 8 - Verificar se todos os organizadores estao na lista
+
+
+# 9 - Tira-los da lista
+
+
+# 10 - fazer uma funçao que calcule o lucro liquido do evento
+# entrada dos parametros sao as vendas e as despesas
+# criar um pacote e gravar o modulo com essa funcao
+# importe o pacote com a funcao
+# chame a funcao
+# execute o programa
+
+
+
+
+
+
+
+
+
+
